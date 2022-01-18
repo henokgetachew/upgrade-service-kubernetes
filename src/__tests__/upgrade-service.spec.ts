69912ff0e11bfa4d5cac23cab3ec83bea0ff75f5
Henok Getachew Alemayehu <henygetachew@gmail.com>
2022-01-18 11:16:23 +0300

    More tests

---
+++ src/__tests__/upgrade-service.spec.ts
@@ -0,0 +1,23 @@
+import { IUpgradeMessage } from "../lib/upgrade-message";
+import { runCommand } from "../utils/command-exec";
+import UpgradeService from '../lib/upgrade-service';
+import { k8s_deployment_name, tempNamespace } from "../resources/test-constants";
+
+
+beforeAll(async () => {
+    await runCommand(`kubectl -n ${tempNamespace} apply -f src/resources/busybox.yaml`, 'Creating a busybox deployment');
+});
+
+describe('Upgrade Service', () => {
+    it('Should upgrade deployment', async () => {
+        const upgradeMessageArray: IUpgradeMessage[] = [{containerName: 'busybox', imageTag: '1.35'}];
+
+        const upgradeService = new UpgradeService(upgradeMessageArray, tempNamespace, k8s_deployment_name );
+        await upgradeService.upgradeDeployment();
+        
+        const result = await upgradeService.getCurrentVersion('busybox');
+        
+        expect(result).toContain('1.35');
+
+    });
+});
