2fca6b9f764c6d26d7543d4525bee70e3c662b52
Henok Getachew Alemayehu <henygetachew@gmail.com>
2022-01-21 16:43:34 +0300

    Feature to test on pre configured cluster

--- src/utils/setup-cluster.js
+++ src/utils/setup-cluster.js
@@ -11,11 +11,18 @@ const initialize = async () => {
     await runCommand(`kubectl -n ${tempNamespace} apply -f kubernetes/rbac/`, 'Applying role based policies...');
 }
 
+const initializeAssumeClusterAlreadyExists = async () => {
+    await runCommand(`kubectl create namespace ${tempNamespace}`, 'Creating test namespace k8s-cht-deployment');
+    await runCommand(`kubectl -n ${tempNamespace} apply -f kubernetes/`, 'Creating deployments...');
+    await runCommand(`kubectl -n ${tempNamespace} apply -f kubernetes/rbac/`, 'Applying role based policies...');
+}
+
 const destroy = async () => {
     await runCommand('k3d cluster delete temporary-test-cluster', 'Deleting temporary test cluster...');
 }
 
 module.exports = {
     initialize,
-    destroy
+    destroy,
+    initializeAssumeClusterAlreadyExists
 }
