bc65a824a6288cf136e0074e2458b6e217ce3f63
Henok Getachew Alemayehu <henygetachew@gmail.com>
2022-02-10 12:30:56 +0300

    Clean up how we load local config

--- src/lib/env-manager.ts
+++ src/lib/env-manager.ts
@@ -1,9 +1,22 @@
-import config from '../../config.json';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
 
 export default class Environment {
+
+    static localConfig(): {'KUBECONFIG_DEFAULT_PATH': string, 'CHT_DEPLOYMENT_NAME': string, 'CHT_NAMESPACE': string} {
+        let config = {
+            'KUBECONFIG_DEFAULT_PATH': '',
+            'CHT_DEPLOYMENT_NAME': '',
+            'CHT_NAMESPACE': ''
+        };
+
+        const localConfigPath = '../../config.json';
+        if(fs.existsSync(localConfigPath)) {
+            config = JSON.parse(fs.readFileSync(localConfigPath).toString());
+        }
+        return config;
+    }
 
     static runningWithinCluster(): boolean {
         if(fs.existsSync('/var/run/secrets/kubernetes.io/serviceaccount/token')) {
@@ -19,7 +32,7 @@ export default class Environment {
 
     static getNamespace(): string {
         let namespace = '';
-        namespace = process.env.CHT_NAMESPACE || config.CHT_NAMESPACE;
+        namespace = process.env.CHT_NAMESPACE || Environment.localConfig().CHT_NAMESPACE;
         if(namespace === '' && Environment.runningWithinCluster()) {
             namespace = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/namespace').toString();
         }
@@ -27,7 +40,7 @@ export default class Environment {
     }
 
     static getDeploymentName(): string {
-        return process.env.CHT_DEPLOYMENT_NAME || config.CHT_DEPLOYMENT_NAME;
+        return process.env.CHT_DEPLOYMENT_NAME || Environment.localConfig().CHT_DEPLOYMENT_NAME;
     }
 
     static getKubeConfigPath(): string {
@@ -36,7 +49,7 @@ export default class Environment {
         } else if (Environment.runningWithinTestAutomation()) { 
             return path.join(os.homedir(), '.kube/config');
         } else {
-            return process.env.KUBECONFIG || config.KUBECONFIG_DEFAULT_PATH;
+            return process.env.KUBECONFIG || Environment.localConfig().KUBECONFIG_DEFAULT_PATH;
         }
     }
 
