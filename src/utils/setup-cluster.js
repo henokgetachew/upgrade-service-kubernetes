bacf913f6fee8f824231b6aef7d0aa2c7bb82655
Henok Getachew Alemayehu <henygetachew@gmail.com>
2022-03-25 13:52:12 +0300

    Style fixes

--- src/utils/setup-cluster.js
+++ src/utils/setup-cluster.js
@@ -5,51 +5,51 @@ const tempCluster = 'temporary-test-cluster';
 const tempNamespace = 'k8s-cht-deployment';
 
 async function configureRBAC() {
-    await runCommand(`kubectl -n ${tempNamespace} apply -f kubernetes/rbac/`, 'Applying role based policies...');
+  await runCommand(`kubectl -n ${tempNamespace} apply -f kubernetes/rbac/`, 'Applying role based policies...');
 }
 
 async function createDeployment() {
-    await runCommand(`kubectl -n ${tempNamespace} apply -f kubernetes/`, 'Creating deployments...');
+  await runCommand(`kubectl -n ${tempNamespace} apply -f kubernetes/`, 'Creating deployments...');
 }
 
 async function createNamespace() {
-    await runCommand(`kubectl create namespace ${tempNamespace}`, 'Creating test namespace k8s-cht-deployment');
+  await runCommand(`kubectl create namespace ${tempNamespace}`, 'Creating test namespace k8s-cht-deployment');
 }
 
 async function importImage() {
-    await runCommand(`k3d image import medicmobile/upgrade-service:latest -c ${tempCluster}`,
-        'Importing docker image...');
+  await runCommand(`k3d image import medicmobile/upgrade-service:latest -c ${tempCluster}`,
+    'Importing docker image...');
 }
 
 async function createCluster() {
-    await runCommand(
-        `k3d cluster create ${tempCluster} --port 5008:30008@loadbalancer`, 'Creating temporary test cluster ...');
+  await runCommand(
+    `k3d cluster create ${tempCluster} --port 5008:30008@loadbalancer`, 'Creating temporary test cluster ...');
 }
 
 async function buildLatestImage() {
-    try {
-        await runCommand(`docker build . -t medicmobile/upgrade-service`, 'Building latest image.');
-    } catch(err) {
-        console.error('Error thrown', err);
-    }
+  try {
+    await runCommand(`docker build . -t medicmobile/upgrade-service`, 'Building latest image.');
+  } catch(err) {
+    console.error('Error thrown', err);
+  }
 }
 
 const initialize = async () => {
-    await buildLatestImage();
-    await createCluster();
-    await importImage();
-    await createNamespace();
-    await createDeployment();
-    await configureRBAC();
-    await runCommand('sleep 5');
+  await buildLatestImage();
+  await createCluster();
+  await importImage();
+  await createNamespace();
+  await createDeployment();
+  await configureRBAC();
+  await runCommand('sleep 5');
 };
 
 const initializeAssumeClusterAlreadyExists = async () => {
-    await buildLatestImage();
-    await importImage();
-    await createNamespace();
-    await createDeployment();
-    await configureRBAC();
+  await buildLatestImage();
+  await importImage();
+  await createNamespace();
+  await createDeployment();
+  await configureRBAC();
 };
 
 const destroy = async () => {
